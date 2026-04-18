ab72b17da3d94db6776e2429bb707f2ec25b89fc
Sincobile <sibisisincobile6@gmail.com>
2026-04-18 17:08:23 +0200

    analysing data using sql; how to update data and partition by

--- sql_script/intermediate_sql.sql
+++ sql_script/intermediate_sql.sql
@@ -7,12 +7,14 @@ select * from [SQL_Tutorial].[dbo].[EmployeeSalary]
 
 --inner join
 --Write a query to return all columns from both EmployeeDemographics and EmployeeSalary where the EmployeeID matches in both tables.
+
 select * from [SQL_Tutorial].[dbo].[EmployeeDemographics]
 inner join [SQL_Tutorial].[dbo].[EmployeeSalary]
 on [SQL_Tutorial].[dbo].[EmployeeDemographics].EmployeeID = [SQL_Tutorial].[dbo].[EmployeeSalary].EmployeeID
 
  
 --Write a query to calculate the average salary of employees with the JobTitle 'Salesman'.
+
 select JobTitle,  AVG (Salary) as average_salary  from [SQL_Tutorial].[dbo].[EmployeeDemographics]
 inner join [SQL_Tutorial].[dbo].[EmployeeSalary]
 on [SQL_Tutorial].[dbo].[EmployeeDemographics].EmployeeID = [SQL_Tutorial].[dbo].[EmployeeSalary].EmployeeID
@@ -51,6 +53,7 @@ on [SQL_Tutorial].[dbo].[EmployeeDemographics].EmployeeID = [SQL_Tutorial].[dbo]
 --right outer join
 
 --Write a query to return all records from EmployeeSalary and matching records from EmployeeDemographics.
+
 select * from [SQL_Tutorial].[dbo].[EmployeeDemographics]
 right outer join [SQL_Tutorial].[dbo].[EmployeeSalary]
 on [SQL_Tutorial].[dbo].[EmployeeDemographics].EmployeeID = [SQL_Tutorial].[dbo].[EmployeeSalary].EmployeeID
@@ -63,6 +66,7 @@ on [SQL_Tutorial].[dbo].[EmployeeDemographics].EmployeeID = [SQL_Tutorial].[dbo]
 --left outer join
 
 --Write a query to return all records from EmployeeDemographics and matching records from EmployeeSalary.
+
 select * from [SQL_Tutorial].[dbo].[EmployeeDemographics]
 left outer join [SQL_Tutorial].[dbo].[EmployeeSalary]
 on [SQL_Tutorial].[dbo].[EmployeeDemographics].EmployeeID = [SQL_Tutorial].[dbo].[EmployeeSalary].EmployeeID
@@ -75,11 +79,13 @@ on [SQL_Tutorial].[dbo].[EmployeeDemographics].EmployeeID = [SQL_Tutorial].[dbo]
 --unions (data is not seperated out)
 
 --Write a query to combine rows from EmployeeDemographics and WareHouseEmployeeDemographics without duplicates.
+
 select * FROM [SQL_Tutorial].[dbo].[EmployeeDemographics]
 union
 select * FROM [SQL_Tutorial].[dbo].[WareHouseEmployeeDemographics]
 
 --Write a query to combine rows from the same tables including duplicates, and sort by EmployeeID.
+
 select * FROM [SQL_Tutorial].[dbo].[EmployeeDemographics]
 union all
 select * FROM [SQL_Tutorial].[dbo].[WareHouseEmployeeDemographics]
@@ -119,9 +125,11 @@ SELECT FirstName, LastName, Age,
   FROM [SQL_Tutorial].[dbo].[EmployeeDemographics]
   where Age is not null
   order by Age
+
   --Write a query to calculate a new salary:
 
 --Write a query to calculate a new salary: (Salesman → +10%,Accountant → +5%, HR → +0.000001% ,Others → +3%)
+
 --Name the new column Salary_After_Raise.
 
   select FirstName, LastName, JobTitle, Salary,
@@ -139,6 +147,7 @@ SELECT FirstName, LastName, Age,
   --Having Clause
 
   --Write a query to count how many employees are in each JobTitle, but only show job titles that appear more than once.
+
   select JobTitle, Count(JobTitle)
   from  [SQL_Tutorial].[dbo].[EmployeeDemographics]
   join [SQL_Tutorial].[dbo].[EmployeeSalary]
@@ -147,6 +156,7 @@ SELECT FirstName, LastName, Age,
   having count(Jobtitle)>1
 
   --Write a query to return job titles where the average salary is greater than 45,000, sorted by average salary.
+
   select JobTitle, Avg(Salary)
   from  [SQL_Tutorial].[dbo].[EmployeeDemographics]
   join [SQL_Tutorial].[dbo].[EmployeeSalary]
@@ -154,4 +164,53 @@ SELECT FirstName, LastName, Age,
   group by JobTitle
   having AVG(salary)> 45000
   order by AVG(Salary)
+
+  --updating and deleting data
+
+  select * from [SQL_Tutorial].[dbo].[EmployeeDemographics]
+
+  update [SQL_Tutorial].[dbo].[EmployeeDemographics]
+  set EmployeeID=1012, Age=31, Gender= 'Female'
+  where FirstName ='holly' and LastName='Flax'
+
+  --note that a delete statement cannot be reversed
+
+  delete from [SQL_Tutorial].[dbo].[EmployeeDemographics]
+  where employeeID=1005
+
+
+  --Aliasing (changing table name to read script more easily, ## more used in aggregate functions)
+  --refrain from using a, b , c = No context
   
+  select FirstName 
+  from [SQL_Tutorial].[dbo].[EmployeeDemographics]
+
+  select FirstName as FN 
+  from [SQL_Tutorial].[dbo].[EmployeeDemographics]
+
+  select FirstName +'  ' +LastName  as FullName 
+  from [SQL_Tutorial].[dbo].[EmployeeDemographics]
+
+  select demo.EmployeeID, Sal.Salary
+  from [SQL_Tutorial].[dbo].[EmployeeDemographics] as demo
+  join [SQL_Tutorial].[dbo].[EmployeeSalary] as Sal
+  on demo.EmployeeID= Sal.EmployeeID
+
+  --Partition by 
+
+
+  select FirstName, LastName, Gender, Salary,
+  count(gender) over (partition by Gender) as Total_Gender
+  from [SQL_Tutorial].[dbo].[EmployeeDemographics] as dem
+join [SQL_Tutorial].[dbo].[EmployeeSalary] as sal
+on dem.EmployeeID=sal.EmployeeID
+
+select  Gender, 
+  count(gender) 
+  from [SQL_Tutorial].[dbo].[EmployeeDemographics] as dem
+join [SQL_Tutorial].[dbo].[EmployeeSalary] as sal
+on dem.EmployeeID=sal.EmployeeID
+group by  Gender
+
+
+ 
